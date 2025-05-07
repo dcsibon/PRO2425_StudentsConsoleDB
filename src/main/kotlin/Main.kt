9c7c2c174b84a26354ea14d38c8045c8dcd11910
dcsibon <dcsibon@gmail.com>
2025-05-07 11:47:32 +0200

    Versión programación con DataSource

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 package es.prog2425.students
 
+import com.zaxxer.hikari.HikariDataSource
 import es.prog2425.students.app.StudentsManager
 import es.prog2425.students.data.dao.StudentDAOH2
 import es.prog2425.students.data.db.DataSourceFactory
 import es.prog2425.students.service.StudentService
 import es.prog2425.students.ui.Console
-import java.sql.SQLException
 
 fun main() {
     // Creo la instancia que gestiona la entrada/salida con el usuario...
@@ -28,4 +28,9 @@ fun main() {
     // Creo la instancia que gestiona el flujo del programa...
     val app = StudentsManager(studentsService, consola)
     app.mostrarMenu()
+
+    // Buenas prácticas en general... evita fugas si se reestructura el código por ejemplo.
+    if (dataSource is HikariDataSource) {
+        dataSource.close()
+    }
 }
